9b09e0269b3d7536403b5c35da6125fbf5999cd9
Alireza <aliareya2003@gmail.com>
2026-08-29 13:23:14 +0430

    ff

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://afghan-areya-bank.vercel.app'],
     credentials: true,
   });
   app.setGlobalPrefix('api');
